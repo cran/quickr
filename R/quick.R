36942e9d531c4baa939c57ad6c6cf12d77689415
Tomasz Kalinowski <tomasz@posit.co>
2025-06-03 12:50:02 +0000

    version 0.1.0

---
+++ R/quick.R
@@ -0,0 +1,235 @@
+#' Compile a Quick Function
+#'
+#' Compile an R function.
+#'
+#' @param fun An R function
+#' @param name Optional string, name to use for the function.
+#'
+#' @details
+#'
+#' ## `declare(type())` syntax:
+#'
+#' The shape and mode of all function arguments must be declared. Local and
+#' return variables may optionally also be declared.
+#'
+#' `declare(type())` also has support for declaring size constraints, or size
+#' relationships between variables. Here are some examples of declare calls:
+#'
+#' ```r
+#' declare(type(x = double(NA))) # x is a 1-d double vector of any length
+#' declare(type(x = double(10))) # x is a 1-d double vector of length 10
+#' declare(type(x = double(1)))  # x is a scalar double
+#'
+#' declare(type(x = integer(2, 3)))  # x is a 2-d integer matrix with dim (2, 3)
+#' declare(type(x = integer(NA, 3))) # x is a 2-d integer matrix with dim (<any>, 3)
+#'
+#' # x is a 4-d logical matrix with dim (<any>, 24, 24, 3)
+#' declare(type(x = logical(NA, 24, 24, 3)))
+#'
+#' # x and y are 1-d double vectors of any length
+#' declare(type(x = double(NA)),
+#'         type(y = double(NA)))
+#'
+#' # x and y are 1-d double vectors of the same length
+#' declare(
+#'   type(x = double(n)),
+#'   type(y = double(n)),
+#' )
+#'
+#' # x and y are 1-d double vectors, where length(y) == length(x) + 2
+#' declare(type(x = double(n)),
+#'         type(y = double(n+2)))
+#' ```
+#'
+#' You can provide declarations to `declare()` as:
+#'
+#' - Multiple arguments to a single `declare()` call
+#' - Separate `declare()` calls
+#' - Multiple arguments within a code block (`{}`) inside `declare()`
+#'
+#' ```r
+#' declare(
+#'   type(x = double(n)),
+#'   type(y = double(n)),
+#' )
+#'
+#' declare(type(x = double(n)))
+#' declare(type(y = double(n)))
+#'
+#' declare({
+#'   type(x = double(n))
+#'   type(y = double(n))
+#' })
+#' ```
+#'
+#' ## Return values
+#'
+#' The shape and type of a function return value must be known at compile time.
+#' In most situations, this will be automatically inferred by `quick()`. However,
+#' if the output is dynamic, then you may need to provide a hint.
+#' For example, returning the result of `seq()` will fail because the output shape
+#' cannot be inferred.
+#'
+#' ```r
+#' # Will fail to compile:
+#' quick_seq <- quick(function(start, end) {
+#'   declare({
+#'     type(start = integer(1))
+#'     type(end = integer(1))
+#'   })
+#'   out <- seq(start, end)
+#'   out
+#' })
+#' ```
+#'
+#' However, if the output size can be declared as a dynamic expression using other
+#' values known at runtime, compilation will succeed:
+#'
+#' ```r
+#' # Succeeds:
+#' quick_seq <- quick(function(start, end) {
+#'   declare({
+#'     type(start = integer(1))
+#'     type(end = integer(1))
+#'     type(out = integer(end - start + 1))
+#'   })
+#'   out <- seq(start, end)
+#'   out
+#' })
+#' quick_seq(1L, 5L)
+#' ```
+#'
+#' @returns A quicker R function.
+#' @export
+#' @examples
+#' add_ab <- quick(function(a, b) {
+#'   declare(type(a = double(n)),
+#'           type(b = double(n)))
+#'   out <- a + b
+#'   out
+#' })
+#' add_ab(1, 2)
+quick <- function(fun, name = NULL) {
+  if (is.null(name)) {
+    name <- if (is.symbol(substitute(fun)))
+      deparse(substitute(fun))
+    else
+      make_unique_name(prefix = "anonymous_quick_function_")
+  }
+
+  if (nzchar(pkgname <- Sys.getenv("DEVTOOLS_LOAD"))) {
+    if (!collector$is_active()) {
+      if (!requireNamespace("pkgload", quietly = TRUE)) {
+        stop("Please install 'pkgload'")
+      }
+      for (i in seq_along(sys.calls())) {
+        if (identical(sys.function(i), pkgload::load_code)) {
+          collector$activate(paste0(pkgname, ":quick_funcs"))
+          defer(dump_collected(), sys.frame(i), after = TRUE)
+          break
+        }
+      }
+    }
+  }
+
+  if (collector$is_active()) {
+    # we are in a quickr::compile_package() or a devtools::load_all() call,
+    # merely collecting functions at this point.
+    quick_closure <- create_quick_closure(name, fun)
+    collector$add(name = name, closure = fun, quick_closure = quick_closure)
+    return(quick_closure)
+  }
+
+  pkgname <- parent.pkg()
+  if (!is.null(pkgname) && pkgname != "quickr") {
+    # we are in a package - but outside a quickr::compile_package() call.
+    return(create_quick_closure(name, fun))
+  }
+
+  # not in a package. Compile and load eagerly.
+  attr(fun, "name") <- name
+  fun <- compile(r2f(fun))
+  attr(fun, "name") <- NULL
+
+  fun
+}
+
+compile <- function(fsub, build_dir = tempfile(paste0(fsub@name, "-build-"))) {
+  stopifnot(inherits(fsub, FortranSubroutine))
+
+  name <- fsub@name
+  c_wrapper <- make_c_bridge(fsub)
+
+  if (dir.exists(build_dir)) unlink(build_dir, recursive = T)
+  if (!dir.exists(build_dir))
+    dir.create(build_dir)
+  owd <- setwd(build_dir)
+  on.exit(setwd(owd))
+
+  fsub_path <- paste0(name, "_fsub.f90")
+  c_wrapper_path <- paste0(name, "_c_wrapper.c")
+  dll_path <- paste0(name, .Platform$dynlib.ext)
+  writeLines(fsub, fsub_path)
+  writeLines(c_wrapper, c_wrapper_path)
+
+  suppressWarnings({
+    result <- system2(
+      R.home("bin/R"),
+      c("CMD SHLIB --use-LTO", "-o", dll_path, fsub_path, c_wrapper_path),
+      stdout = TRUE, stderr = TRUE
+    )
+  })
+  if (!is.null(attr(result, "status"))) {
+    writeLines(result, stderr())
+    str(attributes(result))
+    stop("Compilation Error")
+  }
+
+  # tryCatch(dyn.unload(dll_path), error = identity)
+  dll <- dyn.load(dll_path)
+  c_wrapper_name <- paste0(fsub@name, "_")
+  ptr <- getNativeSymbolInfo(c_wrapper_name, dll)$address
+
+  create_quick_closure(fsub@name, fsub@closure, native_symbol = ptr)
+}
+
+
+
+create_quick_closure <- function(name, closure,
+                                 native_symbol = as.name(paste0(name, "_"))) {
+  body(closure) <- as.call(c(quote(.External), native_symbol,
+                             lapply(names(formals(closure)), as.name)))
+  closure
+}
+
+
+
+check_all_var_names_valid <- function(fun) {
+  nms <- unique(c(names(formals(fun)), all.vars(body(fun), functions = FALSE)))
+  invalid <- endsWith(nms, "_") | startsWith(nms, "_") | nms %in% c(
+
+    # clashes with Fortran subroutine symbols
+    "c_int", "c_double", "c_ptrdiff_t",
+
+    # clashes with C bridge symbols
+    "int" #, "double",
+
+    # ??? (clashes with R symbols?)
+    # "double", "integer"
+  )
+  if (any(invalid)) {
+    stop("symbols cannot start or end with '_', but found: ",
+         glue_collapse(invalid, ", ", last = ", and "))
+  }
+}
+
+
+
+# ---- utils ----
+
+make_unique_name <- local({
+  i <- 0L
+  function(prefix = "tmp") {
+    paste0(prefix, i <<- i + 1L)
+  }
+})
---
+++ tests/testthat/helper.R
@@ -0,0 +1,49 @@
+
+
+scrub_environment <- function(x) {
+  gsub("environment: 0x[0-9a-f]+", "environment: 0x0", x)
+}
+
+expect_translation_snapshots <- function(fn, name = deparse(substitute(fn))) {
+  fn
+  fsub <- r2f(fn)
+  cwrapper <- make_c_bridge(fsub)
+
+  expect_snapshot({
+    fn
+    cat(fsub)
+    cat(cwrapper)
+  }, transform = scrub_environment)
+
+}
+
+expect_quick_identical <- function(fn, ...) {
+  qfn := quick(fn)
+  args_list <- rlang::list2(...)
+  args_list <- lapply(args_list, function(x) if (!is.list(x)) list(x) else x)
+
+  for (args in args_list) {
+    fn_res <- do.call(fn, args)
+    qfn_res <- do.call(qfn, args)
+    expect_identical(fn_res, qfn_res)
+  }
+}
+
+
+expect_quick_equal <- function(fn, ...) {
+  qfn := quick(fn)
+  args_list <- rlang::list2(...)
+  args_list <- lapply(args_list, function(x) if (!is.list(x)) list(x) else x)
+
+  for (args in args_list) {
+    fn_res <- do.call(fn, args)
+    qfn_res <- do.call(qfn, args)
+    expect_equal(fn_res, qfn_res)
+    expect_identical(typeof(fn_res), typeof(qfn_res))
+  }
+}
+
+assign_in_global <- function(...) {
+  x <- rlang::dots_list(..., .named = TRUE)
+  list2env(x, envir = globalenv())
+}
